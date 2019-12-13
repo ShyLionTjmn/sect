ee8594d7bf133309764de2123ec5bdfb2d390e65
Sergey Klyusov <donot@email.me>
2019-12-13 14:22:38 +0500

    minor changes

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 Get indented section of text from stdin by regular expression
 TAB counted as one space
 If matching line ends with opening braces, first non-section line with corresponding closing brace is also printed
+
+Example:
+```# ip addr | sect ens160\.75
+14: ens160.75@ens160: <BROADCAST,MULTICAST,UP,LOWER_UP> mtu 1500 qdisc noqueue state UP group default qlen 1000
+    link/ether 00:0c:29:10:a5:f9 brd ff:ff:ff:ff:ff:ff
+    inet 172.24.56.120/25 brd 172.24.56.127 scope global ens160.75
+       valid_lft forever preferred_lft forever
+    inet6 fe80::20c:29ff:fe10:a5f9/64 scope link 
+       valid_lft forever preferred_lft forever
+```
--- sect.go
+++ sect.go
@@ -9,6 +9,7 @@ import (
 )
 
 func main() {
+
   if len(os.Args) != 2 {
     log.Fatal("Usage: sect pattern")
   }
@@ -18,19 +19,33 @@ func main() {
   scanner := bufio.NewScanner(os.Stdin)
   for scanner.Scan() {
     line := scanner.Text()
+    last_line_printed := false
     //fmt.Printf("\t\"%s\"\n", line)
 M:  if r.MatchString(line) {
-      fmt.Println(line)
+      if !last_line_printed {
+        fmt.Println(line)
+      }
+      last_line_printed = false
       spaces := 0
-      for line[spaces] == ' ' { spaces++ }
+      for line[spaces] == ' ' || line[spaces] == '\t' { spaces++ }
 
       for scanner.Scan() {
         subline := scanner.Text()
         subspaces := 0
-        for subline[subspaces] == ' ' && subspaces <= spaces { subspaces++ }
+        for (subline[subspaces] == ' ' || subline[subspaces] == '\t') && subspaces <= spaces { subspaces++ }
         if subspaces > spaces {
           fmt.Println(subline)
         } else {
+          last_char := line[len(line)-1]
+          first_char := subline[subspaces]
+          if (last_char == '(' && first_char == ')') ||
+             (last_char == '{' && first_char == '}') ||
+             (last_char == '[' && first_char == ']') ||
+             false {
+            //if
+            fmt.Println(subline)
+            last_line_printed = true
+          }
           line = subline
           goto M
         }
